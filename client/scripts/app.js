69eed68b03591043fb8141b79ee50ad8b8c5f11f
b-chou <b_chou@u.pacific.edu>
2016-05-03 12:23:41 -0700

    better design

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,40 +3,49 @@ window.onload = function() {
   initUpdate();
   getRoomNames();
 };
-
 var newRoom = function() {
   var $newroom = $('#createRoom').val();
-  var option = $('<option>' + ($newroom) + '</option>');
-  $('#roomSelect').append(option);
+  var newTabs = $('#tabs').append('<li class=\'active\'>' + $newroom + '</li>');
+  var newTab = $('#tab').append('<li class=\'active\'><div id=\'chats\' name=\'' + $newroom + '\'>' + '</div></li>');
   $('#createRoom').val('');
-  option.prop('selected', true);
+  $("ul#tabs li").click(function(e){
+    if (!$(this).hasClass("active")) {
+      var tabNum = $(this).index();
+      var nthChild = tabNum+1;
+      $("ul#tabs li.active").removeClass("active");
+      $(this).addClass("active");
+      $("ul#tab li.active").removeClass("active");
+      $("ul#tab li:nth-child("+nthChild+")").addClass("active");
+    }
+  });
+  $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+  $.post('https://api.parse.com/1/classes/messages', JSON.stringify({text: 'opened up new room', username: '??', roomname: $newroom}));
 };
 
 var initUpdate = function() {
   setInterval(function() {
-    var room = $('#roomSelect option:selected')[0].text || undefined;
+    var currentTab = $('.active #chats').attr('name');
+    console.log(currentTab);
     $.get('https://api.parse.com/1/classes/messages', function(data) {
-      $('#chats').empty();
+      $('.active #chats').empty();
       var count = 0;
       var i = 0;
       while (i < 100 && count < 10) {
         try {
           if (JSON.stringify(data.results[i])) {
             if (JSON.stringify(data.results[i].username) && JSON.stringify(data.results[i].text) && JSON.stringify(data.results[i].roomname)) {
-              if (escapeRegExp(data.results[i].roomname) === room && escapeRegExp(data.results[i].text).length !== 0) {
+              if (escapeRegExp(data.results[i].roomname) === currentTab) {
                 var username = data.results[i].username;
-
-                // add a button event listener to the nametag of each user, click it to become friends
                 var newMsg = '<div class="msg"><button onclick="addFriend(\'' + username + '\')"><b>' 
-                + escapeRegExp(data.results[i].username) + '</button>:</b> <br>';
+                + escapeRegExp(data.results[i].username) + '</button>:</b>';
 
-                // bolds messages of users who are friends
                 if (friendList.indexOf(username) !== -1) {
-                  newMsg += '<b>' + escapeRegExp(data.results[i].text) + '</b></div><br>';
+                  newMsg += '         <b>' + escapeRegExp(data.results[i].text) + '</b></div><br>';
                 } else {
                   newMsg += escapeRegExp(data.results[i].text) + '</div><br>';
                 }
-                $('#chats').append(newMsg);
+
+                $('.active #chats').append(newMsg);
                 count++;
               }
             }
@@ -58,15 +67,29 @@ var addFriend = function(friend) {
 var getRoomNames = function () {
   var names = [];
   $.get('https://api.parse.com/1/classes/messages', function(data) {
-    $('#roomSelect').empty();
+    $('#tab').empty();
+    $('#tabs').empty();
     for (var i = 1; i < data.results.length; i++) {
       if (JSON.stringify(data.results[i].roomname)) {
-        if (names.indexOf(JSON.stringify(data.results[i].roomname).replace(/\"/g, '')) === -1) {
-          names.push(JSON.stringify(data.results[i].roomname).replace(/\"/g, ''));
-          $('#roomSelect').append('<option>' + JSON.stringify(data.results[i].roomname).replace(/\"/g, '') + '</option>');
+        if (names.indexOf(JSON.stringify(data.results[i].roomname).replace(/\"/g, "")) === -1) {
+          
+          names.push(JSON.stringify(data.results[i].roomname).replace(/\"/g, ""));
+          $('#tabs').append('<li>' + JSON.stringify(data.results[i].roomname).replace(/\"/g, "") + '</li>');
+          $('#tab').append('<li><div id=\'chats\' name=\'' + data.results[i].roomname + '\'>' + '</div></li>');
         }
       }
     }
+    $("ul#tabs li").click(function(e){
+      if (!$(this).hasClass("active")) {
+        var tabNum = $(this).index();
+        var nthChild = tabNum+1;
+        $("ul#tabs li.active").removeClass("active");
+        $(this).addClass("active");
+        $("ul#tab li.active").removeClass("active");
+        $("ul#tab li:nth-child("+nthChild+")").addClass("active");
+      }
+    });
+    $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
   });
 };
 
@@ -75,18 +98,25 @@ var getMessage = function() {
   message.text = $('#message').val();
   $('#message').val('');
   message.username = '';
-  message.roomname = $('#roomSelect option:selected')[0].text;
+  message.roomname = $('.active #chats').attr('name');
+  var flag = false;
   for (var i = document.URL.length - 1; i > 0; i--) {
     if (document.URL[i] === '=') {
       break;
-    } else {
+    } 
+    if (flag) {
       message.username += document.URL[i];
     } 
+    if (document.URL[i] === '#') {
+      flag = true;
+    }
   }
   message.username = message.username.split('').reverse().join('');
   $.post('https://api.parse.com/1/classes/messages', JSON.stringify(message));
 };
 
 var escapeRegExp = function(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 };
+
+
